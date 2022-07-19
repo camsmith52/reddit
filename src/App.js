9d4dbd1bdd95aaf55b6f868d8574b7bc58bd0359
camsmith52 <camsmith52@gmail.com>
2022-07-19 10:50:31 +0200

    api pull2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 import Search from "./Components/Search";
 import AuthContext from "./AuthContext";
 import Content from "./Components/Content";
